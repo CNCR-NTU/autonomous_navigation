5fb8f3d1df5a7e0a0d9291cca99bb6edbed69c2f
Josh Paveley <Joshpaveley@gmail.com>
2019-03-18 16:44:17 +0000

    Basic detection algoirhtm done

--- src/get_laser.py
+++ src/get_laser.py
@@ -3,18 +3,21 @@ import rospy
 import roslib
 
 import math
+import numpy
 
 import geometry_msgs.msg
 import sensor_msgs.msg
 
 
+
 LASER_TOPIC = "/hokuyo_base/scan"
-#Field of view that laser is checking (DEG)
-FIELD_OF_VIEW = 100
+#Field of view that laser is checking in each direction(DEG)
+FIELD_OF_VIEW = 50
 #Level of tolerance for anomalies (%)
-LOT = 5
+LOT = 3
 #Minimum disance for colision detection(M)
-MIN_DISTANCE = 0.7
+WARN_DISTANCE = 1.2
+STOP_DISTANCE = 0.75
 
 class bcolors:
     HEADER = '\033[95m'
@@ -29,42 +32,91 @@ class bcolors:
 
 class laser_scanner:
 
-    def __init__(self,_Topic,_minDis,_FoV,_LoT):
-        self.Topic = _Topic
-        self.minDistance = _minDis
-        self.FoV = _FoV
-        self.tolerance = _LoT
+    COLLISION_LEVEL = ['Path Clear','Collision Imminent','Crashed']
+    WARN_COLOURS = ['\033[92m','\033[93m','\033[91m']
+    VELOCITY_LEVEL = [0.35,0.2,0]
 
-        self.Collision_Imminent = None
+    def __init__(self,_Topic,_warnDis,_stopDis,_FoV,_LoT):
+        self.Topic = _Topic
+        self.warnDis = _warnDis
+        self.stopDis = _stopDis
+        self.FoV = _FoV
+        self.tolerance = _LoT/100.0
+
+        #Array of Laser values
+        self.LaserRange = None
+        self.WarnLVL = 0
+
+        self.avgDist = None
+
+        rospy.init_node('laser', anonymous=True)
+        self.Velocity = rospy.Publisher('/summit_xl_control/cmd_vel', geometry_msgs.msg.Twist,queue_size=1)
 
         self.get_scan()
 
 
     def callback(self,scan):
-
+        
         #get array indices for min and max of range
-        rangeMin = int((len(scan.ranges)/2) - (Deg2Rad(self.FoV)/2)/scan.angle_increment)
-        rangeMax = int((len(scan.ranges)/2) + (Deg2Rad(self.FoV)/2)/scan.angle_increment)
+        rangeMin = int((len(scan.ranges)/2) - (Deg2Rad(self.FoV))/scan.angle_increment)
+        rangeMax = int((len(scan.ranges)/2) + (Deg2Rad(self.FoV))/scan.angle_increment)
 
-        LaserRange = scan.ranges[rangeMin:rangeMax]
+        #Fill array with values from range
+        self.LaserRange = scan.ranges[rangeMin:rangeMax]
 
-        print '\nTotal Scans: {} \t Angle Increment: {}'.format(len(scan.ranges),scan.angle_increment)
-        print '\nRange Min: {} \t Range Mid: {} \t Range Max: {}'.format(rangeMin,len(scan.ranges)/2,rangeMax)
-        print LaserRange[0:10]
-        print len(LaserRange)
+        self.checkScan()
+        self.getFrontDistance(scan.range_max)
+        self.move()
 
-        for i in LaserRange:
-            if i < self.minDistance:
-                print bcolors.WARNING + "Colision!!!" + bcolors.ENDC
-                self.Collision_Imminent = True
-                break
+        self.printInfo()
+      
+    def move(self):
+        cmd = geometry_msgs.msg.Twist()
+        cmd.linear.x = self.VELOCITY_LEVEL[self.WarnLVL]
+        self.Velocity.publish(cmd)
 
-        print('\n---------------------------------------------------------------')    
+    def checkScan(self):
+        lvl2Count = 0
+        lvl3Count = 0
 
+        for i in self.LaserRange:
+            if i < self.stopDis:
+                lvl3Count += 1
+                if lvl3Count > len(self.LaserRange)*self.tolerance:
+                    self.WarnLVL = 2
+                    break
+
+            if i < self.warnDis:
+                lvl2Count += 1
+                if lvl2Count > len(self.LaserRange)*self.tolerance:
+                    self.WarnLVL = 1
+                    break
+
+                self.WarnLVL = 0           
+
+    def getFrontDistance(self,maxDist):
+        avg = 0
+        count = 0
+        for i in self.LaserRange:#[(len(self.LaserRange)/2-30):(len(self.LaserRange)/2+30)]:
+            if i < maxDist:
+                avg += i
+                count += 1
+
+        if count != 0:
+            self.avgDist = avg/count
+
+
+
+    def printInfo(self):
+        print '\n\033[4m' + '\033[1m' + 'Summit_XL Collision Detection' + '\033[0m'
+        print '\nWarn Distance: {}m \t Stop Distance: {}m'.format(self.warnDis,self.stopDis)
+        print 'Average Distance: {}'.format(self.avgDist )
+        print self.WARN_COLOURS[self.WarnLVL] + self.COLLISION_LEVEL[self.WarnLVL] + '\033[0m'
+        print('\n---------------------------------------------------------------')
+
+    
     def get_scan(self):
-        rospy.init_node('laser', anonymous=True)
         rospy.Subscriber(self.Topic, sensor_msgs.msg.LaserScan, self.callback)
-
         rospy.spin()
 
 
@@ -73,7 +125,7 @@ def Deg2Rad(D):
 
 if __name__ == '__main__':
     try:
-        L = laser_scanner(LASER_TOPIC,MIN_DISTANCE,FIELD_OF_VIEW,LOT)
+        L = laser_scanner(LASER_TOPIC,WARN_DISTANCE,STOP_DISTANCE,FIELD_OF_VIEW,LOT)
 
     except rospy.ROSInterruptException:
         pass
