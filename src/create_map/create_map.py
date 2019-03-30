6ab128832ca63e2649c9290047ba32af3f52f119
Josh Paveley <Joshpaveley@gmail.com>
2019-03-30 11:38:29 +0000

    Added Position Tracker

--- src/create_map/create_map.py
+++ src/create_map/create_map.py
@@ -1,18 +1,57 @@
+#!/usr/bin/env python
 import matplotlib.pyplot as plt
 import matplotlib.animation
 import numpy as np
 
-fig, ax = plt.subplots()
-x, y = [],[]
-sc = ax.scatter(x,y)
-plt.xlim(0,10)
-plt.ylim(0,10)
+import rospy
+import roslib
 
-def animate(i):
-    x.append(np.random.rand(1)*10)
-    y.append(np.random.rand(1)*10)
-    sc.set_offsets(np.c_[x,y])
+import geometry_msgs.msg
+import sensor_msgs.msg
+from autonomous_navigation.msg import Position
 
-ani = matplotlib.animation.FuncAnimation(fig, animate, 
-                frames=2, interval=100, repeat=True) 
-plt.show()
+POS_TOPIC = '/summit_xl_controller/position'
+
+
+class create_map:
+
+    def __init__(self,POS_TOPIC):
+        self.__positionTopic = POS_TOPIC
+
+        rospy.init_node('create_map', anonymous=True)
+
+        self.__getPosition()
+
+    
+    def __getPosition(self):
+        rospy.Subscriber(self.__positionTopic, Position, self.__callback)
+        rospy.spin()
+
+    def __callback(self,value):
+        print "\nx: {}".format(value.xPos)
+        print "z: {}".format(value.zPos)
+        print "orientation: {}".format(value.orientation)
+        print "-----------------------------------------"
+
+
+if __name__ == '__main__':
+    try:
+        cm = create_map(POS_TOPIC)
+
+    except rospy.ROSInterruptException:
+                pass
+
+#fig, ax = plt.subplots()
+#x, y = [],[]
+#sc = ax.scatter(x,y)
+#plt.xlim(0,10)
+#plt.ylim(0,10)
+
+#def animate(i):
+#    x.append(np.random.rand(1)*10)
+#    y.append(np.random.rand(1)*10)
+#    sc.set_offsets(np.c_[x,y])
+
+#ani = matplotlib.animation.FuncAnimation(fig, animate, 
+ #               frames=2, interval=100, repeat=True) 
+#plt.show()
--- src/track_position/track_position.py
+++ src/track_position/track_position.py
@@ -5,60 +5,42 @@ import roslib
 import std_msgs.msg
 import geometry_msgs.msg
 import sensor_msgs.msg
+from std_msgs.msg import Float32
+from autonomous_navigation.msg import Position
 
-CMD_TOPIC = '/summit_xl_control/cmd_vel'
-IS_MOVING_TOPIC = '/summit_xl_controller/in_motion'
+POS_TOPIC = '/summit_xl_controller/position'
 
 class position_tracker:
 
-    def __init__(self,cmd,move):
-        self.__cmdTop = cmd
-        self.__moveTop = move
+    def __init__(self,move):
+        self.__positionNode = rospy.Publisher(move, Position,queue_size=1)
+        rospy.init_node('position_tracker', anonymous=True)
 
-        self.__xPos = 0
-        self.__zPos = 0
-        self.__orientation = 0
+        self.__xPos = Float32(float(0.0))
+        self.__zPos = Float32(float(0.0))
+        self.__orientation = Float32(float(0.0))
 
-        self.__inMotion = False
-
-        self.count = 0
-        
         self.__track_pos()
-        
-
-
-    def __IMcallback(self,value):
-        self.__inMotion = value
-
-    def __CMDcallback(self,value):
-        self.__orientation = (self.__orientation + value.linear.y/0.15 * 180/130)%360
-        
-
-        self.count += 1
-
-
-
-        if self.__inMotion:
-            print "\033[92m\n\033[1mIn Motion\033[0m"
-            print "\n\033[1mCurrent Postition:\033[0m"
-            print "\n\033[1mX:\033[0m {} \033[1mZ:\033[0m {} \033[1mOrientation\033[0m: {}".format(self.__xPos,self.__zPos,self.__orientation)    
-            print "\n\033[1mVelocity:\033[0m"
-            print "\n\033[1mLinear:\033[0m x: {} y: {} z: {}".format(value.linear.x,value.linear.y,value.linear.z)
-            print "\033[1mAngular:\033[0m x: {} y: {} z: {}".format(value.angular.x,value.angular.y,value.angular.z)
-            print "Count {}".format(self.count)
-            print('\n---------------------------------------------------------------')
-
 
     def __track_pos(self):
-        rospy.init_node('position_tracker', anonymous=True)
-        rospy.Subscriber(self.__moveTop, std_msgs.msg.Bool, self.__IMcallback)
-        rospy.Subscriber(self.__cmdTop,geometry_msgs.msg.Twist,self.__CMDcallback)
-        rospy.spin()
+
+        msg = Position()
+        msg.orientation = 0.0
+        msg.xPos = 0.0
+        msg.zPos = 0.0
+
+        r = rospy.Rate(10) #10hz
+
+        while not rospy.is_shutdown():
+            self.__positionNode.publish(msg)
+            r.sleep()
+
 
 
 if __name__ == '__main__':
     try:
-        PT = position_tracker(CMD_TOPIC,IS_MOVING_TOPIC)
+        PT = position_tracker(POS_TOPIC)
+        rospy.spin()
 
     except rospy.ROSInterruptException:
-        pass
+                pass
